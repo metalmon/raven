958f8935d24ae57e3b8d7f9dcf5df4b87fd350e5
Metal Monkey <propiska63@gmail.com>
2025-04-03 16:59:53 +0400

    fix(frontend): update Vite config for Raven app start_url and scope

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,8 @@ export default defineConfig(({ command, mode }) => {
 			outDir: "../raven/public/raven",
 			manifest: {
 				name: "Raven",
-				start_url: `/${env.VITE_BASE_NAME}`,
+				start_url: `/raven`,
+				scope: '/raven',
 				short_name: "Raven",
 				description: "Simple, work messaging tool.",
 				display: "standalone",
